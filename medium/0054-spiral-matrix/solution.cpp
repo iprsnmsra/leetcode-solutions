8590f652f2623982caf6e461b98dc872956d17b4
Prasoon Mishra <prasoonmishra9c@gmail.com>
2026-06-23 23:51:56 +0530

    feat: 54. Spiral Matrix [Medium] (C++)

---
+++ medium/0054-spiral-matrix/solution.cpp
@@ -0,0 +1,59 @@
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+      
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+
+        vector<int> result;
+        if (matrix.empty()) return result;
+
+        int m = matrix.size();
+        int n = matrix[0].size();
+        
+       
+        result.reserve(m * n);
+
+        int top = 0;
+        int bottom = m - 1;
+        int left = 0;
+        int right = n - 1;
+
+        while (top <= bottom && left <= right) {
+            
+            for (int j = left; j <= right; ++j) {
+                result.push_back(matrix[top][j]);
+            }
+            top++;
+
+         
+            for (int i = top; i <= bottom; ++i) {
+                result.push_back(matrix[i][right]);
+            }
+            right--; 
+
+
+            if (top <= bottom) {
+                for (int j = right; j >= left; --j) {
+                    result.push_back(matrix[bottom][j]);
+                }
+                bottom--;
+            }
+
+
+            if (left <= right) {
+                for (int i = bottom; i >= top; --i) {
+                    result.push_back(matrix[i][left]);
+                }
+                left++; 
+            }
+        }
+
+        return result;
+    }
+};
