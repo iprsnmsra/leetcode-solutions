b80c98c7fc14bb27b6e194d329654b7daab8d1f6
Prasoon Mishra <prasoonmishra9c@gmail.com>
2026-06-23 23:51:02 +0530

    feat: 1013. Fibonacci Number [Easy] (C++)

---
+++ easy/1013-fibonacci-number/solution.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    int fib(int n) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+
+        int prev2 = 0; // F(0)
+        int prev1 = 1; // F(1)
+
+        for (int i = 2; i <= n; ++i) {
+            int current = prev1 + prev2;
+
+            prev2 = prev1;
+            prev1 = current;
+        }
+
+        return prev1;
+    }
+};
