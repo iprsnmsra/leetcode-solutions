3e386587076ab696f9e9e9dab6c7cb31c92f3964
Prasoon Mishra <prasoonmishra9c@gmail.com>
2026-06-23 23:52:05 +0530

    feat: 48. Rotate Image [Medium] (C++)

---
+++ medium/0048-rotate-image/solution.cpp
@@ -0,0 +1,26 @@
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+
+        int n = matrix.size();
+
+        for (int i = 0; i < n; ++i) {
+
+            for (int j = i + 1; j < n; ++j) {
+                swap(matrix[i][j], matrix[j][i]);
+            }
+        }
+
+        for (int i = 0; i < n; ++i) {
+            reverse(matrix[i].begin(), matrix[i].end());
+        }
+    }
+};
