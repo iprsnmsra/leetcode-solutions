dc0a783afcdb00e70246584ae4194794fbf4e582
Prasoon Mishra <prasoonmishra9c@gmail.com>
2026-06-23 23:52:13 +0530

    feat: 206. Reverse Linked List [Easy] (C++)

---
+++ easy/0206-reverse-linked-list/solution.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ * int val;
+ * ListNode *next;
+ * ListNode() : val(0), next(nullptr) {}
+ * ListNode(int x) : val(x), next(nullptr) {}
+ * ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+
+        ListNode* prev = nullptr;
+        ListNode* curr = head;
+
+        while (curr != nullptr) {
+            ListNode* next_temp = curr->next;
+            
+            curr->next = prev;
+            prev = curr;
+            curr = next_temp;
+        }
+        return prev;
+    }
+};
