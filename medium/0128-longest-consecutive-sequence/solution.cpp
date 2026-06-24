8a884b1f825dd18842c07a2e5dc08ee7eb217699
Prasoon Mishra <prasoonmishra9c@gmail.com>
2026-06-24 21:20:42 +0530

    feat: 128. Longest Consecutive Sequence [Medium] (C++)

---
+++ medium/0128-longest-consecutive-sequence/solution.cpp
@@ -0,0 +1,34 @@
+#include <vector>
+#include <unordered_set>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+
+        unordered_set<int> num_set(nums.begin(), nums.end());
+        int longest_streak = 0;
+
+        for (int num : num_set) {
+
+            if (!num_set.count(num - 1)) {
+                
+                int current_num = num;
+                int current_streak = 1;
+
+                while (num_set.count(current_num + 1)) {
+                    current_num += 1;
+                    current_streak += 1;
+                }
+                longest_streak = max(longest_streak, current_streak);
+            }
+        }
+
+        return longest_streak;
+    }
+};
