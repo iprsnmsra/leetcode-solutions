eba8a1feb992d8fdbe5ed24c4945f2e65fb140d5
Prasoon Mishra <prasoonmishra9c@gmail.com>
2026-06-30 23:21:28 +0530

    feat: 238. Product of Array Except Self [Medium] (C++)

---
+++ medium/0238-product-of-array-except-self/solution.cpp
@@ -0,0 +1,30 @@
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+
+        int n = nums.size();
+
+        vector<int> answer(n, 1);
+
+        int prefix = 1;
+        for (int i = 0; i < n; ++i) {
+            answer[i] = prefix;    
+            prefix *= nums[i];       
+        }
+
+        int suffix = 1;
+        for (int i = n - 1; i >= 0; --i) {
+            answer[i] *= suffix;   
+            suffix *= nums[i];          
+        }
+
+        return answer;
+    }
+};
