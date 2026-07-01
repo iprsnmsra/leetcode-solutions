42de68fb6a54ef4eedbd1c3b60f75edc7b2c4bf6
Prasoon Mishra <prasoonmishra9c@gmail.com>
2026-07-01 19:33:49 +0530

    feat: 41. First Missing Positive [Hard] (C++)

---
+++ hard/0041-first-missing-positive/solution.cpp
@@ -0,0 +1,26 @@
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+
+        int n = nums.size();
+        for (int i = 0; i < n; ++i) {
+            while (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] != nums[i]) {
+                swap(nums[i], nums[nums[i] - 1]);
+            }
+        }
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] != i + 1) {
+                return i + 1;
+            }
+        }
+        return n + 1;
+    }
+};
