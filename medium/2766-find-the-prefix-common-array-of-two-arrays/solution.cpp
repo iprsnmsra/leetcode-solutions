5aab3ef1430866a1e0e9fe548495f61837b4c958
Prasoon Mishra <prasoonmishra9c@gmail.com>
2026-07-05 18:12:00 +0530

    feat: 2766. Find the Prefix Common Array of Two Arrays [Medium] (C++)

---
+++ medium/2766-find-the-prefix-common-array-of-two-arrays/solution.cpp
@@ -0,0 +1,33 @@
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> findThePrefixCommonArray(vector<int>& A, vector<int>& B) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+
+        int n = A.size();
+        vector<int> C(n);
+        
+        vector<int> freq(n + 1, 0); 
+        int common_count = 0;
+
+        for (int i = 0; i < n; ++i) {
+            freq[A[i]]++;
+            if (freq[A[i]] == 2) {
+                common_count++;
+            }
+
+            freq[B[i]]++;
+            if (freq[B[i]] == 2) {
+                common_count++;
+            }
+            C[i] = common_count;
+        }
+
+        return C;
+    }
+};
