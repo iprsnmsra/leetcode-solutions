a0f2906640d1373dfa79dc9e1e4d62110c0324e7
Prasoon Mishra <prasoonmishra9c@gmail.com>
2026-07-09 21:54:51 +0530

    feat: 2509. Minimize XOR [Medium] (C++)

---
+++ medium/2509-minimize-xor/solution.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int minimizeXor(int num1, int num2) {
+        int k = __builtin_popcount(num2);
+        
+        int x = 0;
+
+        for (int i = 31; i >= 0 && k > 0; --i) {
+            if ((num1 >> i) & 1) {
+
+                x |= (1 << i);
+                k--; 
+            }
+        }
+        
+        for (int i = 0; i <= 31 && k > 0; ++i) {
+            if (((x >> i) & 1) == 0) {
+
+                x |= (1 << i);
+                k--; 
+            }
+        }
+        
+        return x;
+    }
+};
