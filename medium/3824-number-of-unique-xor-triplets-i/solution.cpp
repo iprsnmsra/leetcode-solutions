9c3376b7f5c7c4d8154885314386ecd35c236b49
Prasoon Mishra <184728016+iprsnmsra@users.noreply.github.com>
2026-07-24 19:24:31 +0530

    feat: 3824. Number of Unique XOR Triplets I [Medium] (C++)

---
+++ medium/3824-number-of-unique-xor-triplets-i/solution.cpp
@@ -0,0 +1,14 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int uniqueXorTriplets(vector<int>& nums) {
+        
+        int n = nums.size();
+        if (n == 1) return 1;
+        if (n == 2) return 2;
+        return 1 << (32 - __builtin_clz(n));
+    }
+};
