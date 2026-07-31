9f68700f48d904849eae1750edae555dd8bbd307
Prasoon Mishra <184728016+iprsnmsra@users.noreply.github.com>
2026-08-01 00:00:52 +0530

    feat: 450. Delete Node in a BST [Medium] (C++)

---
+++ medium/0450-delete-node-in-a-bst/solution.cpp
@@ -0,0 +1,51 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ * int val;
+ * TreeNode *left;
+ * TreeNode *right;
+ * TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ * TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ * TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* deleteNode(TreeNode* root, int key) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        if (root == nullptr) {
+            return nullptr;
+        }
+        if (key < root->val) {
+            root->left = deleteNode(root->left, key);
+        } 
+        else if (key > root->val) {
+            root->right = deleteNode(root->right, key);
+        } 
+        else {
+            if (root->left == nullptr) {
+                TreeNode* temp = root->right;
+                delete root; 
+                return temp;
+            } 
+            else if (root->right == nullptr) {
+                TreeNode* temp = root->left;
+                delete root; 
+                return temp;
+            }
+            TreeNode* successor = getMin(root->right);
+            root->val = successor->val;
+            root->right = deleteNode(root->right, successor->val);
+        }
+        return root;
+    }
+
+private:
+    TreeNode* getMin(TreeNode* node) {
+        while (node->left != nullptr) {
+            node = node->left;
+        }
+        return node;
+    }
+};
