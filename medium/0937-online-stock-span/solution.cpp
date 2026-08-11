2b9ae87eb0002ef326ed0e183f9ee81fcb2693e4
Prasoon Mishra <184728016+iprsnmsra@users.noreply.github.com>
2026-08-11 21:43:34 +0530

    feat: 937. Online Stock Span [Medium] (C++)

---
+++ medium/0937-online-stock-span/solution.cpp
@@ -0,0 +1,33 @@
+#include <stack>
+#include <utility>
+#include <iostream>
+
+using namespace std;
+
+class StockSpanner {
+private:
+    stack<pair<int, int>> st;
+
+public:
+    StockSpanner() {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+    }
+    
+    int next(int price) {
+        int span = 1; 
+        while (!st.empty() && st.top().first <= price) {
+            span += st.top().second; 
+            st.pop();               
+        }
+        st.push({price, span});
+        
+        return span;
+    }
+};
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner* obj = new StockSpanner();
+ * int param_1 = obj->next(price);
+ */
