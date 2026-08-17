fafb40430ce89be206b445889453cfcd6e8159fa
Prasoon Mishra <184728016+iprsnmsra@users.noreply.github.com>
2026-08-17 21:52:39 +0530

    feat: 165. Compare Version Numbers [Medium] (C++)

---
+++ medium/0165-compare-version-numbers/solution.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int compareVersion(string version1, string version2) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+
+        int n1 = version1.length();
+        int n2 = version2.length();
+        int i = 0, j = 0;
+        while (i < n1 || j < n2) {
+            int num1 = 0;
+            int num2 = 0;
+            while (i < n1 && version1[i] != '.') {
+                num1 = num1 * 10 + (version1[i] - '0');
+                i++;
+            }
+
+            while (j < n2 && version2[j] != '.') {
+                num2 = num2 * 10 + (version2[j] - '0');
+                j++;
+            }
+            if (num1 > num2) return 1;
+            if (num1 < num2) return -1;
+            i++;
+            j++;
+        }
+        return 0;
+    }
+};
