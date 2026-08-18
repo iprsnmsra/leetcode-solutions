96776a2a0ce049ac8158c1b3580b7e4c29c11d72
Prasoon Mishra <184728016+iprsnmsra@users.noreply.github.com>
2026-08-18 19:18:45 +0530

    feat: 1054. Complement of Base 10 Integer [Easy] (C++)

---
+++ easy/1054-complement-of-base-10-integer/solution.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    int bitwiseComplement(int n) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        if (n == 0) return 1;
+        int mask = n;
+        mask |= mask >> 1;
+        mask |= mask >> 2;
+        mask |= mask >> 4;
+        mask |= mask >> 8;
+        mask |= mask >> 16;
+        return n ^ mask;
+    }
+};
