4b15547e968a999d6157d0dbb4d4c282197fa080
Prasoon Mishra <184728016+iprsnmsra@users.noreply.github.com>
2026-08-30 23:09:25 +0530

    feat: 830. Largest Triangle Area [Easy] (C++)

---
+++ easy/0830-largest-triangle-area/solution.cpp
@@ -0,0 +1,31 @@
+#include <vector>
+#include <cmath>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    double largestTriangleArea(vector<vector<int>>& points) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+
+        double max_area = 0.0;
+        int n = points.size();
+        for (int i = 0; i < n - 2; ++i) {
+            for (int j = i + 1; j < n - 1; ++j) {
+                for (int k = j + 1; k < n; ++k) {
+                    double area = 0.5 * abs(
+                        points[i][0] * (points[j][1] - points[k][1]) +
+                        points[j][0] * (points[k][1] - points[i][1]) +
+                        points[k][0] * (points[i][1] - points[j][1])
+                    );
+                    max_area = max(max_area, area);
+                }
+            }
+        }
+
+        return max_area;
+    }
+};
