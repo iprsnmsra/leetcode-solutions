1295af9e60a2d2c4d3410b40fa13c6f50eae01bc
Prasoon Mishra <184728016+iprsnmsra@users.noreply.github.com>
2026-09-01 23:49:57 +0530

    feat: 190. Reverse Bits [Easy] (C++)

---
+++ easy/0190-reverse-bits/solution.cpp
@@ -0,0 +1,22 @@
+#include <cstdint>
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        uint32_t result = 0;
+        
+        for (int i = 0; i < 32; i++) {
+        
+            result = result << 1;
+            if (n & 1) {
+                result++; // or result = result | 1;
+            }
+            n = n >> 1;
+        }
+        
+        return result;
+    }
+};
