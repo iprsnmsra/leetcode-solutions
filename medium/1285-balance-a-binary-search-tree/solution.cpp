f95b61fa895498f5f09e11839f9809980c25fd00
Prasoon Mishra <184728016+iprsnmsra@users.noreply.github.com>
2026-09-05 23:46:17 +0530

    feat: 1285. Balance a Binary Search Tree [Medium] (C++)

---
+++ medium/1285-balance-a-binary-search-tree/solution.cpp
@@ -0,0 +1,51 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ * int val;
+ * TreeNode *left;
+ * TreeNode *right;
+ * TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ * TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ * TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    void inorderTraversal(TreeNode* root, vector<TreeNode*>& nodes) {
+        if (!root) return;
+        inorderTraversal(root->left, nodes);
+        nodes.push_back(root);
+        inorderTraversal(root->right, nodes);
+    }
+
+    TreeNode* buildBalancedBST(vector<TreeNode*>& nodes, int start, int end) {
+        if (start > end) return nullptr;
+
+     
+        int mid = start + (end - start) / 2;
+        TreeNode* root = nodes[mid];
+
+       
+        root->left = buildBalancedBST(nodes, start, mid - 1);
+        root->right = buildBalancedBST(nodes, mid + 1, end);
+
+        return root;
+    }
+
+    TreeNode* balanceBST(TreeNode* root) {
+        
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+
+        vector<TreeNode*> nodes;
+      
+        inorderTraversal(root, nodes);
+
+        return buildBalancedBST(nodes, 0, nodes.size() - 1);
+    }
+};
