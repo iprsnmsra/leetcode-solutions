e9468b77ff30d8f52334cb532100a6ef80f35279
Prasoon Mishra <184728016+iprsnmsra@users.noreply.github.com>
2026-09-19 19:15:06 +0530

    feat: 64. Minimum Path Sum [Medium] (C++)

---
+++ medium/0064-minimum-path-sum/solution.cpp
@@ -0,0 +1,28 @@
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+
+        int m = grid.size();
+        int n = grid[0].size();
+        for (int j = 1; j < n; ++j) {
+            grid[0][j] += grid[0][j - 1];
+        }
+        for (int i = 1; i < m; ++i) {
+            grid[i][0] += grid[i - 1][0];
+        }
+        for (int i = 1; i < m; ++i) {
+            for (int j = 1; j < n; ++j) {
+                grid[i][j] += min(grid[i - 1][j], grid[i][j - 1]);
+            }
+        }
+        return grid[m - 1][n - 1];
+    }
+};
